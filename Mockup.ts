ba02d4b830928c0bb949171397a28cca7d2f1a62
flukejaja <rockliverow90@hotmail.com>
2022-07-10 23:44:20 +0700

    Connect Mongodb

--- Mockup.ts
+++ Mockup.ts
@@ -1,30 +1,39 @@
-interface mockupPrame  {
-    "itemName":string
-    "cateGory":string
-    "Description":string
-    "Drag image here, upload or browse image library":File
-    "Locations":boolean
-    "myBusiness":boolean
-    "Available at all future locations":boolean
+export interface mockupPrame {
+  itemName: string;
+  cateGory: string;
+  Description: string;
+  //   image: File;
+  Locations: boolean;
+  myBusiness: boolean;
+  locations: boolean;
 }
+export const user1: mockupPrame = {
+  itemName: "stone",
+  cateGory: "stone",
+  Description: "stone",
+  Locations: true,
+  myBusiness: false,
+  locations: false,
+};
+
 interface mockupTop {
-    "optionName":string
-    "displayName":string
-    "Options":string
-    "Variation":string
-    "Sku":string
-    "Price":number
-    "createMethod":boolean
-    "addVariations":string
-    "manageStock":string
-    "Track":string
+  optionName: string;
+  displayName: string;
+  Options: string;
+  Variation: string;
+  Sku: string;
+  Price: number;
+  createMethod: boolean;
+  addVariations: string;
+  manageStock: string;
+  Track: string;
 }
-interface mockupFluck{
-    "attributeName":string
-    "Text":string
-    "Selecttion":boolean
-    "Quantity":boolean
-    "Number":number
-    "Precision":number
-    "Toggle":boolean
+interface mockupFluck {
+  attributeName: string;
+  Text: string;
+  Selecttion: boolean;
+  Quantity: boolean;
+  Number: number;
+  Precision: number;
+  Toggle: boolean;
 }
--- index.ts
+++ index.ts
@@ -1,18 +1,40 @@
-import fastify from 'fastify'
-import { MongoClient } from 'mongodb'
-const url = "mongodb://127.0.0.1:27017"
-const Client = new MongoClient(url)
+import fastify from "fastify";
+import { dbPrame, dbTop, dbFluke } from "./server";
+import { user1 } from "./Mockup";
 
-const app = fastify()
+const app = fastify();
 
-app.get('/ping', async (request , reply) => {
-  return 'pong\n'
-})
+// const testPrame = async () => {
+//   const db = await dbPrame.find({}).toArray();
+//   console.log("dbPrame Connection = " + JSON.stringify(db));
+// };
+// const testTop = async () => {
+//   const db = await dbTop.find({}).toArray();
+//   console.log("dbTop Connection = " + JSON.stringify(db));
+// };
 
+// const testFluke = async () => {
+//   const db = await dbFluke.find({}).toArray();
+//   console.log("dbFluke Connection = " + JSON.stringify(db));
+// };
+// console.log(testPrame());
+// console.log(testFluke());
+// console.log(testTop());
+
+// app.post("/inSertitem", async (request, reply) => {
+
+//   console.log()
+//   // const db = await dbPrame.insertOne(mockupPrame);
+// });
+
+app.get("/getdb", async (request, reply) => {
+  console.log("Connecting to getdb");
+  reply.send("TestGetDB" + JSON.stringify(user1));
+});
 app.listen({ port: 5000 }, (err, address) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
-})
+  console.log(`Server listening at ${address}`);
+});
