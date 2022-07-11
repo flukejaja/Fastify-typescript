f11748a33c67d81ea95a19449238f4a75a80ea9c
flukejaja <rockliverow90@hotmail.com>
2022-07-11 17:18:26 +0700

    Add db Prame Top Fluck

--- db/server.ts
+++ db/server.ts
@@ -4,3 +4,6 @@ const Client = new MongoClient(url);
 export const connect = Client.connect();
 export const db = Client.db("local");
 export const dbCollection = db.collection("dbTest");
+export const dbPrame = db.collection("dbPrame");
+export const dbTop = db.collection("dbTop");
+export const dbFluck = db.collection("dbFluck");
