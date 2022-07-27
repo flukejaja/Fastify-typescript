3f13ae0ae6c1642e3ea64930a46a354075e42abf
flukejaja <rockliverow90@hotmail.com>
2022-07-27 11:20:05 +0700

    try-catch

--- account/Dashboard.ts
+++ account/Dashboard.ts
@@ -1,8 +1,13 @@
 import { dbMock } from "../db/server";
 
 export const dashboard = async () => {
-  const db = await dbMock
-    .find({}, { projection: { _id: 0, email: 1, phone: 1, id: 1, name: 1 } })
-    .toArray();
-  return db;
+ try {
+   const db = await dbMock
+     .find({}, { projection: { _id: 0, email: 1, phone: 1, id: 1, name: 1 } })
+     .toArray();
+     if(!db)return {message:"Error",succes: false}
+   return db;
+ } catch (error) {
+  return { succes: false, error: error}
+ }
 };
--- account/delAttribute.ts
+++ account/delAttribute.ts
@@ -1,14 +1,18 @@
 import { dbFluck } from "../db/server";
 
 export const delAttribute = async (body: any) => {
-  const result = await dbFluck.deleteOne(body);
-  console.log("result => ", result);
-  if (!result) {
-    return { success: false };
+  try {
+    const result = await dbFluck.deleteOne(body);
+    console.log("result => ", result);
+    if (!result) {
+      return { success: false };
+    }
+    const results = {
+      success: true,
+      data: result,
+    };
+    return results;
+  } catch (error) {
+    return { success: false , messages: error };
   }
-  const results = {
-    success: true,
-    data: result,
-  };
-  return results;
 };
